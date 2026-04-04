296d0c7c3fbd148866cb83ef6268508d6447e002
filki <filipjanwojciech@gmail.com>
2026-04-04 20:57:22 +0200

    Added fb bot webhooks

---
+++ src/kwiaciarnia_monika/bots/fb_bot.py
@@ -0,0 +1,38 @@
+from this import d
+import fastapi
+from fastapi import Query
+from fastapi.responses import Response
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = fastapi.FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/webhook")
+def webhook(
+    mode: str = Query(..., alias="hub.mode"),
+    token: str = Query(..., alias="hub.verify_token"),
+    challenge: str = Query(None, alias="hub.challenge"),
+):
+    if mode == "subscribe" and token == os.getenv("FB_VERIFY_TOKEN"):
+        return Response(content=challenge, status_code=200)
+    return Response(content="Invalid verification", status_code=403)
+
+
+@app.post("/webhook")
+def webhook_post(sender_info: dict):
+    print(sender_info)
+    return Response(content="OK", status_code=200)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
