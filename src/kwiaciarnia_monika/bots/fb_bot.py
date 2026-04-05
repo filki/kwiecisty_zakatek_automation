aa0726ac872b181ccfd48e23fbe9f9b916a03ce3
filki <filipjanwojciech@gmail.com>
2026-04-05 14:39:36 +0200

    Added fb bot

--- src/kwiaciarnia_monika/bots/fb_bot.py
+++ src/kwiaciarnia_monika/bots/fb_bot.py
@@ -1,13 +1,15 @@
-from this import d
 import fastapi
 from fastapi import Query
 from fastapi.responses import Response
 import os
 from dotenv import load_dotenv
+import httpx
 
 load_dotenv()
 
+META_URL = "https://graph.facebook.com/v25.0/me/messages"
 app = fastapi.FastAPI()
+httpx_client = httpx.AsyncClient()
 
 
 @app.get("/")
@@ -21,16 +23,64 @@ def webhook(
     token: str = Query(..., alias="hub.verify_token"),
     challenge: str = Query(None, alias="hub.challenge"),
 ):
-    if mode == "subscribe" and token == os.getenv("FB_VERIFY_TOKEN"):
-        print(f"DEBUG - Od FB: '{token}', Z pliku ENV: '{os.getenv('FB_TOKEN')}'")
+    if mode == "subscribe" and token == os.getenv("FB_TOKEN"):
         return Response(content=challenge, status_code=200)
     return Response(content="Invalid verification", status_code=403)
 
 
 @app.post("/webhook")
-def webhook_post(sender_info: dict):
-    print(sender_info)
+async def webhook_post(sender_info: dict):
+    message = sender_info["entry"][0]["messaging"][0]["message"].get("text")
+    message_clean = "" if message is None else message.lower().strip()
+    sender_id = sender_info["entry"][0]["messaging"][0]["sender"]["id"]
+    if not message_clean:
+        return Response(content="OK", status_code=200)
+    if "cześć" in message_clean:
+        await _send_message(
+            sender_id,
+            "Witamy w pracowni florystycznej Kwiecisty Zakątek! Jestem Twoim asystentem, w czym mogę dzisiaj służyć?",
+        )
+        await _send_options(sender_id)
+    else:
+        await _send_message(sender_id, "Nie rozumiem")
     return Response(content="OK", status_code=200)
+
+
+async def _send_message(sender_id: str, message: str):
+    response = await httpx_client.post(
+        url=META_URL,
+        params={"access_token": os.getenv("FB_PAGE_ACCESS_TOKEN")},
+        json={"recipient": {"id": sender_id}, "message": {"text": message}},
+    )
+    print(f"STATUS: {response.status_code}")
+    print(f"BODY: {response.text}")
+
+
+async def _send_options(sender_id: str):
+    response = await httpx_client.post(
+        url=META_URL,
+        params={"access_token": os.getenv("FB_PAGE_ACCESS_TOKEN")},
+        json={
+            "recipient": {"id": sender_id},
+            "message": {
+                "text": "Oto Twoje opcje do wyboru:",
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "test 1",
+                        "payload": "UKRYTA_WARTOSC_1",
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "test 2",
+                        "payload": "UKRYTA_WARTOSC_2",
+                    },
+                ],
+            },
+        },
+    )
+    print(f"STATUS: {response.status_code}")
+    print(f"BODY: {response.text}")
 
 
 if __name__ == "__main__":
